55082109550e51f426824c24f9b5ce9ac6670804
Raja Muhammad Taha <rajataha151@gmail.com>
2026-02-10 10:59:11 +0500

    Add Employee class with salary management

---
+++ 16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+private:
+    int salary;
+
+public:
+    void setSalary(int s) {
+        salary = s;
+    }
+
+    int getSalary() {
+        return salary;
+    }
+};
+
+int main() {
+    Employee e;
+    e.setSalary(30000);
+
+    cout << "Salary = " << e.getSalary();
+    return 0;
+}
