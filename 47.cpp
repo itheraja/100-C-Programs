aac39f675664b58163281232b29f506f88b806c8
Raja Muhammad Taha <rajataha151@gmail.com>
2026-02-10 11:16:04 +0500

    Create 47.cpp

---
+++ 47.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+public:
+    int real, imag;
+
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    Complex operator + (const Complex &obj) {
+        Complex res;
+        res.real = real + obj.real;
+        res.imag = imag + obj.imag;
+        return res;
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(2, 3), c2(4, 5);
+    Complex c3 = c1 + c2;
+
+    c3.display();
+    return 0;
+}
