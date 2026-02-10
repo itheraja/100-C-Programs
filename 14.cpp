eef746c3e6ec8cb1e7b926b20847878871ae3801
Raja Muhammad Taha <rajataha151@gmail.com>
2026-02-10 10:58:18 +0500

    Implement basic Calculator class with add and sub methods

---
+++ 14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int sub(int a, int b) {
+        return a - b;
+    }
+};
+
+int main() {
+    Calculator c;
+    cout << "Addition = " << c.add(5, 3) << endl;
+    cout << "Subtraction = " << c.sub(5, 3);
+
+    return 0;
+}
